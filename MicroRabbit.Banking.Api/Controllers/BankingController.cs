3c1ff2de0bb84533c25c8faf5cf69171340ec45a
Tobbe3108 <Tobbe3108@gmail.com>
2020-04-06 22:54:39 +0200

    BankingController

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Account>>> Get()
+        {
+            var accounts = await _accountService.GetAccounts();
+            if (accounts == null) return BadRequest();
+            if (!accounts.Any()) return NotFound();
+            return Ok(accounts);
+        }
+    }
+}
